5f12cfa356ea2babcc49b29b3acc2c5f7782864e
Kevin Gao <kevinmingjiang@gmail.com>
2021-11-07 19:53:16 +0800

    hook the backend

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from "./axios"; // Make sure we import local axios here
 
 function TinderCards() {
-	const [people, setPeople] = useState([
-		{
-			name: "Elon Musk",
-			url: "https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg",
-		},
-		{
-			name: "Jeff Bezos",
-			url: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F61840baec89a6d1193df915f%2FUS-IT-lifestyle-Amazon-internet-technology-economy-computers%2F960x0.jpg%3Ffit%3Dscale",
-		},
-	]);
+	const [people, setPeople] = useState([]);
+
+	// Hook the backend
+	// Call functions after DOM updates or rendering
+	useEffect(() => {
+		async function fetchData() {
+			const req = await axios.get("/tinder/cards");
+			setPeople(req.data);
+		}
+		fetchData();
+	}, []);
+
+	console.log(people);
 
 	const swiped = (direction, nameToDelete) => {
 		console.log("removing: " + nameToDelete);
@@ -32,7 +36,7 @@ function TinderCards() {
 						preventSwipe={["up", "down"]}
 						onSwipe={(dir) => swiped(dir, person.name)}
 						onCardLeftScreen={() => outOfFrame(person.name)}>
-						<div style={{ backgroundImage: `url(${person.url})` }} className="card">
+						<div style={{ backgroundImage: `url(${person.imgUrl})` }} className="card">
 							<h3>{person.name}</h3>
 						</div>
 					</TinderCard>
